cd0254a0ff06b3a63c18cbab09de1b962558dfbc
Prachi kad <prachikadpatil04@gmail.com>
2025-12-24 22:16:21 +0530

    Add files via upload

---
+++ contactform/script.js
@@ -0,0 +1,18 @@
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  if (name === "" || email === "" || message === "") {
+    alert("All fields are required");
+    return false;
+  }
+
+  if (!email.includes("@")) {
+    alert("Enter valid email");
+    return false;
+  }
+
+  alert("Form submitted successfully!");
+  return true;
+}
